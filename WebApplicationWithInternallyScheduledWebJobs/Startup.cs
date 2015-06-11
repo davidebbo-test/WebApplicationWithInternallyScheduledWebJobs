01be0cd2d4954f69762187e8e090be39e71ebd81
davidebbo <david.ebbo@microsoft.com>
2015-06-11 11:48:43 -0700

    Initial commit

---
+++ WebApplicationWithInternallyScheduledWebJobs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationWithInternallyScheduledWebJobs.Startup))]
+namespace WebApplicationWithInternallyScheduledWebJobs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
